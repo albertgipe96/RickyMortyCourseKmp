c4513fdddb13174c353fc594db24fffc63c9bba5
albertgipe96 <albertgipe96@gmail.com>
2024-10-11 16:14:35 +0200

    Fix App in MainActivity

--- composeApp/src/androidMain/kotlin/com/example/rickymortycoursekmp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/example/rickymortycoursekmp/MainActivity.kt
@@ -11,13 +11,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            RickMortyApp()
+            App()
         }
     }
-}
-
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    RickMortyApp()
 }
